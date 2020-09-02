07d9a508a04c23772cff5d3e8223125777fe1814
Szotyi Lehet <32148855+szotyi41@users.noreply.github.com>
2020-09-02 11:16:46 +0200

    Api data and click url

--- dynamic.content.js
+++ dynamic.content.js
@@ -37,193 +37,206 @@
 
 // contentFields: Object of placeholders e.g. {headline_1: 'Buy now', background_image_1: '', ...}
 function replaceContent(contentFields) {
-    
-    var elements = [];
-    var value = "";
-    var placeholder = "";
-    var selector = "";
-    var placeholders = Object.keys(contentFields);
+	
+	var elements = [];
+	var value = "";
+	var placeholder = "";
+	var selector = "";
+	var placeholders = Object.keys(contentFields);
 
-    console.log("Dynamic Content loaded: ", contentFields);
+	console.log("Dynamic Content loaded: ", contentFields);
 
-    // Each in placeholders
-    for (var i in placeholders) {
-        placeholder = placeholders[i];
-        value = contentFields[placeholder];
-        selector = [
-            "#" + placeholder,
-            "." + placeholder,
-            "#" + placeholder.replace(/_/g, '-'), 
-            "." + placeholder.replace(/_/g, '-')
-        ].join(', ');
+	// Each in placeholders
+	for (var i in placeholders) {
+		placeholder = placeholders[i];
+		value = contentFields[placeholder];
+		selector = [
+			"#" + placeholder,
+			"." + placeholder,
+			"#" + placeholder.replace(/_/g, '-'), 
+			"." + placeholder.replace(/_/g, '-')
+		].join(', ');
 
-        elements = document.querySelectorAll(selector);
+		elements = document.querySelectorAll(selector);
 
-        // If queried elements not found for placeholder
-        if (!elements || !elements.length) {
-            console.warn("Elements not found for placeholder: ", placeholder, " selector: " + selector);
-            continue;
-        }
+		// If queried elements not found for placeholder
+		if (!elements || !elements.length) {
+			console.warn("Elements not found for placeholder: ", placeholder, " selector: " + selector);
+			continue;
+		}
 
-        // Each in DOM elements
-        for (var e = 0, element; element = elements[e]; e++) {
-            try {
+		// Each in DOM elements
+		for (var e = 0, element; element = elements[e]; e++) {
+			try {
 
-                // Replace images
-                if (placeholder.indexOf("image") !== -1) {
-                    // If tag is img set src if not set background image
-                    if (element.tagName.toLowerCase() === "img") {
-                        console.log(placeholder, "image placeholder found, set src to: ", value.Url);
-                        element.src = value.Url;
-                    } else {
-                        console.log(placeholder, "image placeholder found, set background image to: ", value.Url);
-                        element.style.backgroundImage = "url(\'" + value.Url + "\')";
-                    }
-                    continue;
-                }
+				// Replace images
+				if (placeholder.indexOf("image") !== -1) {
+					// If tag is img set src if not set background image
+					if (element.tagName.toLowerCase() === "img") {
+						console.log(placeholder, "image placeholder found, set src to: ", value.Url);
+						element.src = value.Url;
+					} else {
+						console.log(placeholder, "image placeholder found, set background image to: ", value.Url);
+						element.style.backgroundImage = "url(\'" + value.Url + "\')";
+					}
+					continue;
+				}
 
-                // Replace urls if tag a
-                if (placeholder.indexOf("url") !== -1) {
-                    if (element.tagName.toLowerCase() === "a") {
-                        var url = value.Url ? value.Url : value;
-                        console.log(placeholder, "url placeholder found, set href to: ", url);
-                        element.href = url;
-                    }
-                    continue;
-                }
+				// Replace urls
+				if (placeholder.indexOf("url") !== -1) {
+					var url = value.Url ? value.Url : value;
 
-                // Replace colors
-                if (placeholder.indexOf("color") !== -1) {
-                    console.log(placeholder, "color placeholder found, set color to: ", value);
-                    if (placeholder.indexOf("background") !== -1) {
-                        element.style.backgroundColor = value;
-                    } else {
-                        element.style.color = value;
-                    }
-                    continue;
-                }
+					// If tag nam is <a> set href
+					if (element.tagName.toLowerCase() === "a") {
+						console.log(placeholder, "url placeholder found, set href to: ", url);
+						element.href = url;
+						continue;
+					}
 
-                // Replace postions
-                if (placeholder.indexOf("position") !== -1) {
-                    console.log(placeholder, "positioning placeholder found, set background position to: ", value);
-                    element.style.cssText += value;
-                    continue;
-                }
+					// Else its a click_url set onAdClick event
+					element.onclick = function(event) {
+					  onAdClickEvent(event, url);
+					}
+				}
 
-                // Hide element
-                if (placeholder.indexOf("visible") !== -1) {
-                    console.log(placeholder, "is ", value, ", " + (value ? "show" : "hide") + " elements");
-                    element.style.display = value;
-                    continue;
-                }
+				// Replace colors
+				if (placeholder.indexOf("color") !== -1) {
+					console.log(placeholder, "color placeholder found, set color to: ", value);
+					if (placeholder.indexOf("background") !== -1) {
+						element.style.backgroundColor = value;
+					} else {
+						element.style.color = value;
+					}
+					continue;
+				}
 
-                // Set opacity
-                if (placeholder.indexOf("opacity") !== -1) {
-                    console.log(placeholder, "opacity placeholder found");
-                    element.style.opacity = value;
-                    continue;
-                }
+				// Replace postions
+				if (placeholder.indexOf("position") !== -1) {
+					console.log(placeholder, "positioning placeholder found, set background position to: ", value);
+					element.style.cssText += value;
+					continue;
+				}
 
-                // Import css file
-                if (placeholder.indexOf("css") !== -1) {
-                    var file = location.pathname.split( "/" ).pop();
-                    var link = document.createElement("link");
-                    link.href = value;
-                    link.type = "text/css";
-                    link.rel = "stylesheet";
-                    document.getElementsByTagName("head")[0].appendChild(link);
-                }
+				// Hide element
+				if (placeholder.indexOf("visible") !== -1) {
+					console.log(placeholder, "is ", value, ", " + (value ? "show" : "hide") + " elements");
+					element.style.display = value;
+					continue;
+				}
 
-                // Set css
-                if (placeholder.indexOf("style") !== -1) {
-                    console.log(placeholder, "css placeholder found");
-                    element.style.cssText += value;
-                    continue;
-                }
+				// Set opacity
+				if (placeholder.indexOf("opacity") !== -1) {
+					console.log(placeholder, "opacity placeholder found");
+					element.style.opacity = value;
+					continue;
+				}
 
-                // Openweathermap api url
-                if (placeholder.indexOf("weather") !== -1) {
-                    callWeatherApi(element, value);
-                    continue;
-                }
+				// Import css file
+				if (placeholder.indexOf("css") !== -1) {
+					var file = location.pathname.split( "/" ).pop();
+					var link = document.createElement("link");
+					link.href = value;
+					link.type = "text/css";
+					link.rel = "stylesheet";
+					document.getElementsByTagName("head")[0].appendChild(link);
+				}
 
-                // Replace text placeholders
-                if (typeof value === 'string') {
-                    console.log(placeholder, "placeholder found, set inner text to: ", value);
-                    element.innerHTML = value.toString().replace(/\n/g, "<br>");
-                    continue;
-                }
+				// Set css
+				if (placeholder.indexOf("style") !== -1) {
+					console.log(placeholder, "css placeholder found");
+					element.style.cssText += value;
+					continue;
+				}
 
-                console.log(placeholder, "placeholder not used (DOM not changed)");
+				// Call api and get info
+				if (placeholder.indexOf("api") !== -1) {
+					callAPI(element, value);
+					continue;
+				}
+
+				// Replace text placeholders
+				if (typeof value === 'string') {
+					console.log(placeholder, "placeholder found, set inner text to: ", value);
+					element.innerHTML = value.toString().replace(/\n/g, "<br>");
+					continue;
+				}
+
+				console.log(placeholder, "placeholder not used (DOM not changed)");
 
 
-            } catch (error) {
-                console.error("Error found when replace placeholder: ", placeholder, "Element: ", element, "Error Message: ", error);
-            }
-        }
-    }
+			} catch (error) {
+				console.error("Error found when replace placeholder: ", placeholder, "Element: ", element, "Error Message: ", error);
+			}
+		}
+	}
+}
+
+// Trigger click tag. DO NOT USE WITH <a> tag
+function onAdClickEvent(e, url) {
+	console.log('Click tag triggered', url);
+	Enabler.exitOverride('Click tag', url);
 }
 
 /*  
-* Get weather api url
+* Call api url
 */
-function callWeatherApi(element, url = 'https://api.openweathermap.org/data/2.5/weather?q=budapest&units=metric&APPID=&show=main.temp') {
+function callAPI(element, url = 'https://api.openweathermap.org/data/2.5/weather?q=budapest&units=metric&APPID=&show=main.temp') {
 
-    // For current URL: https://api.openweathermap.org/data/2.5/weather?q=budapest&units=metric&APPID=&show=main.temp
-    // For daily URl: 
-    if (!url) {
-        console.error('Weather api url is not defined');
-        return;
-    }
+	// For current URL: https://api.openweathermap.org/data/2.5/weather?q=budapest&units=metric&APPID=&show=main.temp
+	// For daily URl: 
+	if (!url) {
+		console.error('Weather api url is not defined');
+		return;
+	}
 
-    try {
-        var value;
+	try {
+		var value;
 
-        // Get parameters to show
-        var urlParams = new URLSearchParams(url); 
+		// Get parameters to show
+		var urlParams = new URLSearchParams(url); 
 
-        // Get show parameters
-        var showParam = urlParams.get('show'); 
+		// Get show parameters
+		var showParam = urlParams.get('show'); 
 
-        // Array of ['main', 'temp'] or ['current', '2', 'temp']
-        var showInCreative = showParam.split('.');
+		// Array of ['main', 'temp'] or ['current', '2', 'temp']
+		var showInCreative = showParam.split('.');
 
-        var xhttp = new XMLHttpRequest();
-        xhttp.onreadystatechange = function() {
-            if (this.readyState == 4 && this.status == 200) {
-                var json = JSON.parse(xhttp.responseText);
-                if (typeof json === 'object') {
+		var xhttp = new XMLHttpRequest();
+		xhttp.onreadystatechange = function() {
+			if (this.readyState == 4 && this.status == 200) {
+				var json = JSON.parse(xhttp.responseText);
+				if (typeof json === 'object') {
 
-                    console.log('Weather data received', json);
+					console.log('API data received', json);
 
-                    // Get the value (find) main.temp
-                    value = json[showInCreative[0]];
-                    for (var i = 0; i < showInCreative.length; i++) {
-                        if (value[showInCreative[i]]) {
-                            value = value[showInCreative[i]];
-                        } else {
-                            console.error('Failed to get weather info from parameter ', showInCreative);
-                            return;
-                        }
-                    }
+					// Get the value (find) main.temp
+					value = json[showInCreative[0]];
+					for (var i = 0; i < showInCreative.length; i++) {
+						if (value[showInCreative[i]]) {
+							value = value[showInCreative[i]];
+						} else {
+							console.error('Failed to get data ', showInCreative);
+							return;
+						}
+					}
 
-                    // Load data
-                    if (value) {
-                        console.log('Weather for today loaded: ', value);
-                        element.innerHTML = value;
-                        return;
-                    }
+					// Set data in html
+					if (value) {
+						console.log('Data set in DOM: ', value);
+						element.innerHTML = value;
+						return;
+					}
 
-                    console.error('Failed to load weather data: ', 'There are no data for the day');
-                }
+					console.error('Failed to load data from api');
+				}
 
-            } else {
-                console.error('Failed to load weather data: ' + this.status);
-            }
-        };
-        xhttp.open("GET", url, true);
-        xhttp.send();
-    } catch (error) {
-        console.error('Failed to load weather data: ' + error);
-    }
+			} else {
+				console.error('Failed to load API data: ' + this.status);
+			}
+		};
+		xhttp.open("GET", url, true);
+		xhttp.send();
+	} catch (error) {
+		console.error('Failed to load API data: ' + error);
+	}
 }
